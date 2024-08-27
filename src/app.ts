2f2e00cee34411029205f11344cb480f76ff5f86
Dara Javaherian <darajavaherian@gmail.com>
2024-08-27 14:09:51 +0200

    implement infinite scrolling

--- src/app.ts
+++ src/app.ts
@@ -15,264 +15,14 @@ app.use(express.json());
 
 let connection: mysql.Connection;
 
-const flagMap = {
-  AF: "Afghanistan",
-  AX: "Åland Islands",
-  AL: "Albania",
-  DZ: "Algeria",
-  AS: "American Samoa",
-  AD: "Andorra",
-  AO: "Angola",
-  AI: "Anguilla",
-  AQ: "Antarctica",
-  AG: "Antigua and Barbuda",
-  AR: "Argentina",
-  AM: "Armenia",
-  AW: "Aruba",
-  AU: "Australia",
-  AT: "Austria",
-  AZ: "Azerbaijan",
-  BS: "Bahamas",
-  BH: "Bahrain",
-  BD: "Bangladesh",
-  BB: "Barbados",
-  BY: "Belarus",
-  BE: "Belgium",
-  BZ: "Belize",
-  BJ: "Benin",
-  BM: "Bermuda",
-  BT: "Bhutan",
-  BO: "Bolivia",
-  BQ: "Bonaire, Sint Eustatius and Saba",
-  BA: "Bosnia and Herzegovina",
-  BW: "Botswana",
-  BV: "Bouvet Island",
-  BR: "Brazil",
-  IO: "British Indian Ocean Territory",
-  BN: "Brunei Darussalam",
-  BG: "Bulgaria",
-  BF: "Burkina Faso",
-  BI: "Burundi",
-  CV: "Cabo Verde",
-  KH: "Cambodia",
-  CM: "Cameroon",
-  CA: "Canada",
-  KY: "Cayman Islands",
-  CF: "Central African Republic",
-  TD: "Chad",
-  CL: "Chile",
-  CN: "China",
-  CX: "Christmas Island",
-  CC: "Cocos (Keeling) Islands",
-  CO: "Colombia",
-  KM: "Comoros",
-  CG: "Congo",
-  CD: "Democratic Republic of the Congo",
-  CK: "Cook Islands",
-  CR: "Costa Rica",
-  CI: "Côte d'Ivoire",
-  HR: "Croatia",
-  CU: "Cuba",
-  CW: "Curaçao",
-  CY: "Cyprus",
-  CZ: "Czechia",
-  DK: "Denmark",
-  DJ: "Djibouti",
-  DM: "Dominica",
-  DO: "Dominican Republic",
-  EC: "Ecuador",
-  EG: "Egypt",
-  SV: "El Salvador",
-  GQ: "Equatorial Guinea",
-  ER: "Eritrea",
-  EE: "Estonia",
-  SZ: "Eswatini",
-  ET: "Ethiopia",
-  FK: "Falkland Islands",
-  FO: "Faroe Islands",
-  FJ: "Fiji",
-  FI: "Finland",
-  FR: "France",
-  GF: "French Guiana",
-  PF: "French Polynesia",
-  TF: "French Southern Territories",
-  GA: "Gabon",
-  GM: "Gambia",
-  GE: "Georgia",
-  DE: "Germany",
-  GH: "Ghana",
-  GI: "Gibraltar",
-  GR: "Greece",
-  GL: "Greenland",
-  GD: "Grenada",
-  GP: "Guadeloupe",
-  GU: "Guam",
-  GT: "Guatemala",
-  GG: "Guernsey",
-  GN: "Guinea",
-  GW: "Guinea-Bissau",
-  GY: "Guyana",
-  HT: "Haiti",
-  HM: "Heard Island and McDonald Islands",
-  VA: "Holy See",
-  HN: "Honduras",
-  HK: "Hong Kong",
-  HU: "Hungary",
-  IS: "Iceland",
-  IN: "India",
-  ID: "Indonesia",
-  IR: "Iran",
-  IQ: "Iraq",
-  IE: "Ireland",
-  IM: "Isle of Man",
-  IL: "Israel",
-  IT: "Italy",
-  JM: "Jamaica",
-  JP: "Japan",
-  JE: "Jersey",
-  JO: "Jordan",
-  KZ: "Kazakhstan",
-  KE: "Kenya",
-  KI: "Kiribati",
-  KP: "North Korea",
-  KR: "Korea",
-  KW: "Kuwait",
-  KG: "Kyrgyzstan",
-  LA: "Lao People's Democratic Republic",
-  LV: "Latvia",
-  LB: "Lebanon",
-  LS: "Lesotho",
-  LR: "Liberia",
-  LY: "Libya",
-  LI: "Liechtenstein",
-  LT: "Lithuania",
-  LU: "Luxembourg",
-  MO: "Macao",
-  MG: "Madagascar",
-  MW: "Malawi",
-  MY: "Malaysia",
-  MV: "Maldives",
-  ML: "Mali",
-  MT: "Malta",
-  MH: "Marshall Islands",
-  MQ: "Martinique",
-  MR: "Mauritania",
-  MU: "Mauritius",
-  YT: "Mayotte",
-  MX: "Mexico",
-  FM: "Micronesia",
-  MD: "Moldova",
-  MC: "Monaco",
-  MN: "Mongolia",
-  ME: "Montenegro",
-  MS: "Montserrat",
-  MA: "Morocco",
-  MZ: "Mozambique",
-  MM: "Myanmar",
-  NA: "Namibia",
-  NR: "Nauru",
-  NP: "Nepal",
-  NL: "Netherlands",
-  NC: "New Caledonia",
-  NZ: "New Zealand",
-  NI: "Nicaragua",
-  NE: "Niger",
-  NG: "Nigeria",
-  NU: "Niue",
-  NF: "Norfolk Island",
-  MK: "North Macedonia",
-  MP: "Northern Mariana Islands",
-  NO: "Norway",
-  OM: "Oman",
-  PK: "Pakistan",
-  PW: "Palau",
-  PS: "Palestine, State of",
-  PA: "Panama",
-  PG: "Papua New Guinea",
-  PY: "Paraguay",
-  PE: "Peru",
-  PH: "Philippines",
-  PN: "Pitcairn",
-  PL: "Poland",
-  PT: "Portugal",
-  PR: "Puerto Rico",
-  QA: "Qatar",
-  RE: "Réunion",
-  RO: "Romania",
-  RU: "Russian Federation",
-  RW: "Rwanda",
-  BL: "Saint Barthélemy",
-  SH: "Saint Helena",
-  KN: "Saint Kitts and Nevis",
-  LC: "Saint Lucia",
-  MF: "Saint Martin (French part)",
-  PM: "Saint Pierre and Miquelon",
-  VC: "Saint Vincent and the Grenadines",
-  WS: "Samoa",
-  SM: "San Marino",
-  ST: "Sao Tome and Principe",
-  SA: "Saudi Arabia",
-  SN: "Senegal",
-  RS: "Serbia",
-  SC: "Seychelles",
-  SL: "Sierra Leone",
-  SG: "Singapore",
-  SX: "Sint Maarten (Dutch part)",
-  SK: "Slovakia",
-  SI: "Slovenia",
-  SB: "Solomon Islands",
-  SO: "Somalia",
-  ZA: "South Africa",
-  GS: "South Georgia and the South Sandwich Islands",
-  SS: "South Sudan",
-  ES: "Spain",
-  LK: "Sri Lanka",
-  SD: "Sudan",
-  SR: "Suriname",
-  SJ: "Svalbard and Jan Mayen",
-  SE: "Sweden",
-  CH: "Switzerland",
-  SY: "Syrian Arab Republic",
-  TW: "Taiwan",
-  TJ: "Tajikistan",
-  TZ: "Tanzania",
-  TH: "Thailand",
-  TL: "Timor-Leste",
-  TG: "Togo",
-  TK: "Tokelau",
-  TO: "Tonga",
-  TT: "Trinidad and Tobago",
-  TN: "Tunisia",
-  TR: "Turkey",
-  TM: "Turkmenistan",
-  TC: "Turks and Caicos Islands",
-  TV: "Tuvalu",
-  UG: "Uganda",
-  UA: "Ukraine",
-  AE: "United Arab Emirates",
-  GB: "UK",
-  US: "USA",
-  UM: "United States Minor Outlying Islands",
-  UY: "Uruguay",
-  UZ: "Uzbekistan",
-  VU: "Vanuatu",
-  VE: "Venezuela",
-  VN: "Viet Nam",
-  VG: "Virgin Islands (British)",
-  VI: "Virgin Islands (U.S.)",
-  WF: "Wallis and Futuna",
-  EH: "Western Sahara",
-  YE: "Yemen",
-  ZM: "Zambia",
-  ZW: "Zimbabwe",
-};
-
 app.get("/", async (req: Request, res: Response) => {
   const ip = await getIp(req);
 
   if (!ip) {
     return res.status(400).json({ error: "No IP provided" });
   }
+
+  // await populateDummy();
 
   // if (await hasSigned(ip)) {
   //   const removeSql = `DELETE FROM guestbook WHERE ip = ?`;
@@ -282,14 +32,75 @@ app.get("/", async (req: Request, res: Response) => {
   if (await hasSigned(ip)) {
     return res.render("index", {
       ip,
-      flagMap,
-      guestbook: await getGuestbook(),
+
+      guestbook: await getGuestbookPage(0, (await getIp(req)) as string),
     });
   } else {
     return res.render("add", {
       ip,
     });
   }
+});
+
+const populateDummy = async () => {
+  let ii = 0;
+  for (let j = 100; j < 200; j++) {
+    for (let i = 100; i < 255; i++) {
+      const countries = ["de", "br", "us", "ru", "fr", "es", "it", "jp", "cn"];
+
+      const randomWords = [
+        "Hi",
+        "Hello",
+        "Howdy",
+        "Yo",
+        "Hey",
+        "Yo",
+        "App",
+        "Apple",
+        "Can",
+        "Take",
+        "have",
+        "your",
+        "name",
+        "please",
+      ];
+      const checkSql = `SELECT * FROM guestbook WHERE ip = ?`;
+
+      const [results] = await connection.query(checkSql, [`19.214.${i}.${j}`]);
+      if ((results as any[]).length > 0) {
+        continue;
+      }
+
+      const sql = `INSERT INTO guestbook (ip, countryCode, message) VALUES (?, ?, ?)`;
+
+      const nRandomWords = (n: number) => {
+        const words = [];
+        for (let i = 0; i < n; i++) {
+          words.push(
+            randomWords[Math.floor(Math.random() * randomWords.length)]
+          );
+        }
+        return words.join(" ").slice(0, 256);
+      };
+
+      const result = await connection.query(sql, [
+        `19.214.${i}.${j}`,
+        countries[Math.floor(Math.random() * countries.length)],
+        ii++ + nRandomWords(Math.floor(Math.random() * 100) + 1),
+      ]);
+
+      // console.log(result);
+    }
+  }
+};
+
+app.get("/page/:page(\\d+)", async (req: Request, res: Response) => {
+  const page = parseInt(req.params.page);
+
+  return res.render("page", {
+    guestbook: await getGuestbookPage(page, (await getIp(req)) as string),
+    ip: await getIp(req),
+  });
 });
 
 app.post("/add", async (req: Request, res: Response) => {
@@ -362,8 +173,19 @@ const addToDb = async (ip: string, countryCode: string, message?: string) => {
   }
 };
 
-const getGuestbook = async () => {
-  const sql = `SELECT * FROM guestbook`;
+const getGuestbookPage = async (page: number, ip: string) => {
+  console.log(`page ${page} ip ${ip}`);
+  const pageSize = 100;
+  const offset = Math.abs(page * pageSize);
+
+  const sql = `SELECT *
+  FROM (
+      SELECT *
+      FROM guestbook
+      ORDER BY id DESC
+      LIMIT ${pageSize} OFFSET ${offset}
+  ) subquery
+  ORDER BY id ASC;`;
   const result = await connection.query(sql);
   return result[0];
 };
@@ -381,7 +203,7 @@ const initDb = async () => {
     ip VARCHAR(45) UNIQUE NOT NULL,
     countryCode CHAR(2) NOT NULL,
     createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    message VARCHAR(256),
+    message VARCHAR(1000),
     INDEX (ip)
   );`;
 
