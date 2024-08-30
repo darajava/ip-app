66b2a2b0836e237584cf8574d29bdfe2b5c853ab
Dara Javaherian <darajavaherian@gmail.com>
2024-08-30 19:11:43 +0200

    add pm2 script

--- pm2.sh
+++ pm2.sh
@@ -7,16 +7,13 @@ npm install
 
 echo "Building..."
 # Run the build command with increased memory limit
-NODE_OPTIONS='--max-old-space-size=4096' npx tsc
+NODE_OPTIONS='--max-old-space-size=4096' npm run build
 
 # Check if the build command was successful
 if [ $? -ne 0 ]; then
   echo "Build failed. Exiting."
   exit 1
 fi
-
-# Copy the environment file and locale files
-cp .env ./build
 
 # Restart pm2 and show logs if build was successful
 pm2 restart all && pm2 logs
